71964418a870ace5c083a2211e9d4b013acff1cd
Kirill Ivanov <kirdmiv@gmail.com>
2022-11-06 04:01:39 +0100

    Modified landing page

--- web/src/components/LandingPage/LandingPage.tsx
+++ web/src/components/LandingPage/LandingPage.tsx
@@ -4,8 +4,30 @@ import React, { FC } from 'react';
 interface LandingPageProps {}
 
 const LandingPage: FC<LandingPageProps> = () => (
-  <div>
-    LandingPage Component
+  <div className="text-center">
+      <p className="display-6">
+          GoGreener aims to encourage people to buy more environmentally
+          friendly goods and commit actions to improve the ecological situation.
+      </p>
+
+      <p className="display-6">
+          Our project helps different companies to cooperate. Some companies are willing to give some
+          benefits to people who care about the environment. However,
+          there are a lot of unknown organizations which are trying to save our planet.
+          Our team would like to help people find such organizations and contribute to
+          sustainability. In addition, GoGreener motivates people via virtual
+          currency -- "Green Points" plus a profile page that people can share on social media.
+      </p>
+
+      <p className="display-6">
+          How does it work?
+          Organizations can add promotions such as "Get 100 points for every 20 euros on the
+          bill" or "Get 5 points for every recycled bottle". Other organizations can add
+          rewards such as "Get 10 euro gift card for 100 points". After contributing enough,
+          the user can redeem accumulated points and receive benefits.
+          We believe that organizations will be pleased to participate in our project because such
+          motivation will involve many more people in fighting against environmental problems.
+      </p>
   </div>
 );
 
