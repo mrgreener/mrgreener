5a4b4464d8f9ea72ec5335ccd9b4725e7d8de723
Mark Ipatov <markthehopeful@gmail.com>
2022-11-06 04:44:26 +0100

    Basic populate

---
+++ populate.sql
@@ -0,0 +1,6 @@
+INSERT INTO `Client` (`authId`, `username`, `name`, `isAdmin`, `registeredOn`, `bio`, `avatarUrl`)
+VALUES
+    ("XdWjHaOwII23", "Alex42", "Alexander", FALSE, "2022-10-01 01:00:00", "Student @ BIT", "https://de.wikipedia.org/wiki/Alex_Pettyfer#/media/Datei:Alex_Pettyfer_2006_(252479969)_cropped.jpg"),
+    ("kSAudf67qCsf", "M4rk", "Mark Ipatov", TRUE, "2022-09-25 12:30:00", "2nd year Student @ JUB", "https://s1.zerochan.net/Penny.Polendina.600.1628923.jpg"),
+    ("kGFks11elsif", "petrtsv", "Tsvetkov Petr", FALSE, "2022-10-03 11:49:12", "Go Green!!", "https://sun9-east.userapi.com/sun9-24/s/v1/ig2/9NAmhLElVsO_w49wZDmo7TBmqF7Hl_JGuwSUt4E6BCK8s9QpzFd7AzK00UklT0UapZkROIRjuqqYKIJc3itT3RYe.jpg?size=1440x2160&quality=95"),
+    ("fewuh3r3124x", "kirdmv", "Kirill Ivanov", TRUE, "2022-10-04 11:49:12", "Hi, I am Kirill", NULL),
