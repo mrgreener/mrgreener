2f82c57141e646fbc4b4a6fb163156097466812d
Mark Ipatov <markthehopeful@gmail.com>
2022-11-06 05:32:37 +0100

    one more populate

--- populate.sql
+++ populate.sql
@@ -12,4 +12,16 @@ INSERT INTO `Organizations` (`authId`, `username`, `name`, `apiKey`, `verified`,
 VALUES ("9w&x0Xq31wo3", "avocadoshop", "Avocado Shop", "wi3f89s74zue72", TRUE,
         "Best Avocado Shop in the town",
         "https://imgmedia.lbb.in/media/2019/11/5dcae73a53703c2f98a1da63_1573578554772.jpg",
+        "Campus Ring 1, Bremen, Germany", "https://www.avocadostore.de", "inbox@avocadostore.de", "2022-11-06T00:49"),
+       ("fuwer2yh8e87", "Shein", "Shein Shop", "1143oo12peoxkf", TRUE,
+        "The great Shein Shop",
+        "https://www.textilwirtschaft.de/news/media/23/Klarna--Shein-Pop-up-Shop-in-London-225385-detailp.png",
+        "Schadowstraße 23", "https://roe.shein.com/", "inbox@sheinstore.de", "2022-11-01T13:29"),
+       ("OE7387hfdO*&", "avocadoshop", "Avocado Shop", "wi3f89s74zue72", TRUE, --dup!
+        "Best Avocado Shop in the town",
+        "https://imgmedia.lbb.in/media/2019/11/5dcae73a53703c2f98a1da63_1573578554772.jpg",
+        "Campus Ring 1, Bremen, Germany", "https://www.avocadostore.de", "inbox@avocadostore.de", "2022-11-06T00:49"),
+       ("L#Q@O*E(UPQ)", "avocadoshop", "Avocado Shop", "wi3f89s74zue72", TRUE, --dup!
+        "Best Avocado Shop in the town",
+        "https://imgmedia.lbb.in/media/2019/11/5dcae73a53703c2f98a1da63_1573578554772.jpg",
         "Campus Ring 1, Bremen, Germany", "https://www.avocadostore.de", "inbox@avocadostore.de", "2022-11-06T00:49");
