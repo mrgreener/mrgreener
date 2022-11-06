ba9169d0fbc33d6e0048c2c5a80cbd105a612d2e
Mark Ipatov <markthehopeful@gmail.com>
2022-11-06 05:08:31 +0100

    populate progress

--- populate.sql
+++ populate.sql
@@ -1,6 +1,15 @@
 INSERT INTO `Client` (`authId`, `username`, `name`, `isAdmin`, `registeredOn`, `bio`, `avatarUrl`)
-VALUES
-    ("XdWjHaOwII23", "Alex42", "Alexander", FALSE, "2022-10-01 01:00:00", "Student @ BIT", "https://de.wikipedia.org/wiki/Alex_Pettyfer#/media/Datei:Alex_Pettyfer_2006_(252479969)_cropped.jpg"),
-    ("kSAudf67qCsf", "M4rk", "Mark Ipatov", TRUE, "2022-09-25 12:30:00", "2nd year Student @ JUB", "https://s1.zerochan.net/Penny.Polendina.600.1628923.jpg"),
-    ("kGFks11elsif", "petrtsv", "Tsvetkov Petr", FALSE, "2022-10-03 11:49:12", "Go Green!!", "https://sun9-east.userapi.com/sun9-24/s/v1/ig2/9NAmhLElVsO_w49wZDmo7TBmqF7Hl_JGuwSUt4E6BCK8s9QpzFd7AzK00UklT0UapZkROIRjuqqYKIJc3itT3RYe.jpg?size=1440x2160&quality=95"),
-    ("fewuh3r3124x", "kirdmv", "Kirill Ivanov", TRUE, "2022-10-04 11:49:12", "Hi, I am Kirill", NULL),
+VALUES ("XdWjHaOwII23", "Alex42", "Alexander", FALSE, "2022-10-01 01:00:00", "Student @ BIT",
+        "https://de.wikipedia.org/wiki/Alex_Pettyfer#/media/Datei:Alex_Pettyfer_2006_(252479969)_cropped.jpg"),
+       ("kSAudf67qCsf", "M4rk", "Mark Ipatov", TRUE, "2022-09-25 12:30:00", "2nd year Student @ JUB",
+        "https://s1.zerochan.net/Penny.Polendina.600.1628923.jpg"),
+       ("kGFks11elsif", "petrtsv", "Tsvetkov Petr", FALSE, "2022-10-03 11:49:12", "Go Green!!",
+        "https://sun9-east.userapi.com/sun9-24/s/v1/ig2/9NAmhLElVsO_w49wZDmo7TBmqF7Hl_JGuwSUt4E6BCK8s9QpzFd7AzK00UklT0UapZkROIRjuqqYKIJc3itT3RYe.jpg?size=1440x2160&quality=95"),
+       ("fewuh3r3124x", "kirdmv", "Kirill Ivanov", TRUE, "2022-10-04 11:49:12", "Hi, I am Kirill", NULL);
+
+INSERT INTO `Organizations` (`authId`, `username`, `name`, `apiKey`, `verified`, `description`, `avatarUrl`, `location`,
+                             `siteUrl`, `contactEmail`, `registeredOn`)
+VALUES ("9w&x0Xq31wo3", "avocadoshop", "Avocado Shop", "wi3f89s74zue72", TRUE,
+        "Best Avocado Shop in the town",
+        "https://imgmedia.lbb.in/media/2019/11/5dcae73a53703c2f98a1da63_1573578554772.jpg",
+        "Campus Ring 1, Bremen, Germany", "https://www.avocadostore.de", "inbox@avocadostore.de", "2022-11-06T00:49");
